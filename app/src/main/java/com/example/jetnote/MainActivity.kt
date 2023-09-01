6bc3a01ee3d2a7d2fe89e464cb408398e2cc49a7
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-09-01 20:45:45 +0900

    Prepare Hilt settings

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetnote.screen.NoteScreen
 import com.example.jetnote.screen.NoteViewModel
 import com.example.jetnote.ui.theme.JetNoteTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
